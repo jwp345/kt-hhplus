97f22e714f1e593175666e9635bc79791a0ed26f
jwp345 <jwp135@naver.com>
2023-12-04 20:40:06 +0900

    feat: actuator로 healthCheck 구현

---
+++ src/main/kotlin/com/hhplus/HhplusApplication.kt
@@ -0,0 +1,11 @@
+package com.hhplus
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HhplusApplication
+
+fun main(args: Array<String>) {
+    runApplication<HhplusApplication>(*args)
+}
