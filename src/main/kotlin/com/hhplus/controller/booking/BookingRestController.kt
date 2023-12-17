34f32786cc444df9f5c861acdf508097f077e173
jwp345 <jwp135@naver.com>
2023-12-17 13:58:47 +0900

    fix : 1~50 까지의 좌석 숫자 아닐 경우 예외 처리 및 테스트

--- src/main/kotlin/com/hhplus/controller/booking/BookingRestController.kt
+++ src/main/kotlin/com/hhplus/controller/booking/BookingRestController.kt
@@ -2,18 +2,24 @@ package com.hhplus.controller.booking
 
 import com.hhplus.controller.ApiResponse
 import com.hhplus.service.booking.BookingService
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.lang.IllegalArgumentException
 
 @RestController
 @RequestMapping("/api/v1/booking")
 class BookingRestController (val bookingService: BookingService){
 
     @GetMapping("/dates/available")
-    fun findDatesAvailable(@RequestParam(required = true) seatId: Int) : ApiResponse<AvailableDateDto> {
-        return ApiResponse.ok(bookingService.findDatesAvailable(seatId))
+    fun findDatesAvailable(@RequestParam(required = true) seatId: Int) : ApiResponse<AvailableDateDto?> {
+        try {
+            return ApiResponse.ok(bookingService.findDatesAvailable(seatId))
+        } catch (e: IllegalArgumentException) {
+            return ApiResponse(4001, HttpStatus.BAD_REQUEST, "좌석은 1~50까지만 존재합니다.", null)
+        }
     }
 
 }
