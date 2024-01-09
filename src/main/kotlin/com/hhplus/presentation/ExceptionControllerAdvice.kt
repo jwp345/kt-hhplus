a978bc68678cd2a20e17209429c56d8e7f2c1a22
jwp345 <jwp135@naver.com>
2024-01-09 10:31:05 +0900

    fix : 에러 로그 보이게 설정 추가

--- src/main/kotlin/com/hhplus/presentation/ExceptionControllerAdvice.kt
+++ src/main/kotlin/com/hhplus/presentation/ExceptionControllerAdvice.kt
@@ -1,14 +1,22 @@
 package com.hhplus.presentation
 
 import com.hhplus.domain.exception.InvalidException
+import mu.KotlinLogging
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 
 @RestControllerAdvice
-class ExceptionControllerAdvice {
+class ExceptionControllerAdvice() {
 
+    private val log = KotlinLogging.logger("GlobalExceptionHandler")
     @ExceptionHandler
     fun <T> handleIllegalStateException(ex: InvalidException): ApiResponse<T?> {
         return ApiResponse.error(code = ex.code, message = ex.message)
+    }
+
+    @ExceptionHandler
+    fun <T> handleGlobalException(ex : Exception): ApiResponse<T?> {
+        log.error { ex.stackTrace }
+        return ApiResponse.error(code = 500, message = "알 수 없는 에러가 발생 하였습니다.")
     }
 }
