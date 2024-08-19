fecab781864567846ba6c05a59cb5db92eec0396
jwp345 <jwp135@naver.com>
2024-08-20 02:24:55 +0900

    fix: 비동기 트랜잭션(결제 이력) 실패 시 처리
    재시도 로직은 어차피 db에 부하가 걸렸으면 재시도를 해도 안될 거기 때문에 로깅만 남겨놓도록 변경

--- src/main/kotlin/com/hhplus/component/PaymentEventListener.kt
+++ src/main/kotlin/com/hhplus/component/PaymentEventListener.kt
@@ -1,6 +1,7 @@
 package com.hhplus.component
 
 import com.hhplus.domain.repository.PaymentRepository
+import mu.KotlinLogging
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionalEventListener
@@ -8,10 +9,16 @@ import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
 class PaymentEventListener(val paymentRepository: PaymentRepository) {
-    /* TODO : 실패 시 재시도 로직과 로깅 처리 */
+
+    private val log = KotlinLogging.logger("PaymentEventListener")
+
     @Async
     @TransactionalEventListener
     fun savePayments(paymentEvent: PaymentEvent) {
-        paymentRepository.saveAll(paymentEvent.payments);
+        try {
+            paymentRepository.saveAll(paymentEvent.payments)
+        } catch (ex: Exception) {
+            log.error { ex.message }
+        }
     }
 }
